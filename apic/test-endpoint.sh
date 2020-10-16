63639f3a4c90b1e95599040480a23fc5f3a63965
Jerome Tarte <jtarte@Jeromes-MacBook-Pro-2.local>
2020-10-16 17:27:54 +0200

    test api

--- apic/test-endpoint.sh
+++ apic/test-endpoint.sh
@@ -2,7 +2,9 @@
 RC=$(curl -k -s -o /dev/null -w "%{http_code}" -H 'x-ibm-client-id: '$2'' --header 'accept: application/json' $1)
 if [ $RC -eq 200 ]
 then
+    echo "call API succeded. HTTP code" $RC
     exit 0
 else
+    echo "call API failed. HTTP code" $RC 
     exit 1
 fi

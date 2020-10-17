fe5fbcae2737ca25dc51dde8c52e1ffe258581a6
Jerome Tarte <jtarte@Jeromes-MacBook-Pro-2.local>
2020-10-17 21:04:45 +0200

    mesh interface upgrade

---
+++ gitops/ibmcloud/test_frontend_gold_v2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIRECTORY=mesh-apic-demo
+MYPATH=$(pwd | sed 's/\('$DIRECTORY'\).*/\1/g')
+
+for i in `seq 1 $1`
+do
+    curl -H "mode:gold" -H 'version: v2' --cacert $MYPATH/ca-cert/159.8.94.146.nip.io.crt https://cloud-frontend.159.8.94.146.nip.io/invoke 
+    sleep 1
+    echo ""
+done;
